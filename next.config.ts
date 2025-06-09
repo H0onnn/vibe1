d3848ee76457843990aa14a84e17899ee636ce9a
H0onnn <dukei2017@naver.com>
2025-06-09 15:26:14 +0900

    chore: stagewise

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages:
+    process.env.NODE_ENV === "development" ? ["@stagewise/toolbar-next"] : [],
 };
 
 export default nextConfig;
